eb531a8fc19d5a977f3dfdd83d9c1dcbeb809686
Kyle Hess <kyle@kylehess.com>
2026-07-15 13:18:50 -0700

    Remove floating TanStack devtools badge from every screen

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,4 @@
 import { HeadContent, Scripts, createRootRoute } from '@tanstack/react-router'
-import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
-import { TanStackDevtools } from '@tanstack/react-devtools'
 
 import appCss from '../styles.css?url'
 import { IntakeProvider } from '#/store/intake-context'
@@ -53,17 +51,6 @@ function RootDocument({ children }: { children: React.ReactNode }) {
             <Footer />
           </div>
         </IntakeProvider>
-        <TanStackDevtools
-          config={{
-            position: 'bottom-right',
-          }}
-          plugins={[
-            {
-              name: 'Tanstack Router',
-              render: <TanStackRouterDevtoolsPanel />,
-            },
-          ]}
-        />
         <Scripts />
       </body>
     </html>
